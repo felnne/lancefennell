007f446016669a1ed6f2ad293b31d1f395c6dfc3
Felix Fennell <felnne@bas.ac.uk>
2024-09-20 11:55:29 +0100

    Add forward/back nav for detail pages

--- src/lancefennell/app.py
+++ src/lancefennell/app.py
@@ -2,8 +2,23 @@ from flask import Flask, redirect, url_for, render_template
 
 from lancefennell.data import data
 
+def _get_surrounding_items(slug: str):
+    previous_item = None
+    current_item = None
+    next_item = None
 
-def category_items(category: str):
+    for i, item in enumerate(data):
+        if item['slug'] == slug:
+            current_item = item
+            if i > 0:
+                previous_item = data[i - 1]
+            if i < len(data) - 1:
+                next_item = data[i + 1]
+            break
+
+    return previous_item, current_item, next_item
+
+def _category_items(category: str):
     items = [item for item in data if item['category'] == category]
     return render_template("items.j2", items=items, category=category)
 
@@ -22,11 +37,11 @@ def create_app():
 
     @app.route('/<category>')
     def items(category: str) -> str:
-        return category_items(category=category)
+        return _category_items(category=category)
 
     @app.route('/items/<slug>')
     def detail(slug: str) -> str:
-        item = next((item for item in data if item['slug'] == slug), None)
-        return render_template("item.j2", item=item)
+        previous_item, current_item, next_item = _get_surrounding_items(slug)
+        return render_template("item.j2", item=current_item, previous=previous_item, next=next_item)
 
     return app
